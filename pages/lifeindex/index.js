3108a38d653b1b5d5dc826e4e1e93219798be633
wangtianrun <m17888819538@163.com>
2018-12-18 16:26:56 +0800

    实现微信小程序全部功能并发布

---
+++ pages/lifeindex/index.js
@@ -0,0 +1,21 @@
+// pages/lifeindex/lifeindex/index.js 
+var app = getApp(); 
+Page({   
+  data: {}, 
+
+  onLoad: function (options) {//程序启动时执行 onLoad     
+    wx.setNavigationBarTitle({//设置标题       
+      title: '生活指数'    
+    })     
+    let lifeindex = wx.getStorageSync('life');//取出标签为 life 的数据     
+    //console.log(lifeindex);     
+    this.setData({//渲染到 wxml       
+      lifeindex: lifeindex     
+    })   
+  },   
+  /**    
+   *  用户点击右上角分享    
+   */   
+  onShareAppMessage: function () { 
+  } 
+})
